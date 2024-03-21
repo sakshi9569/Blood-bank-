f2dfad674243c8027aa8ab1729239c1145be9d59
Vandana56s <vandana5697s@gmail.com>
2024-03-21 15:48:05 +0530

    added userModel,authRoutes,authControllers,authMiddlewares,include jsonwebtoken,bcrypt for authentication

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const colors = require("colors");
 
 const connectDB = async () => {
@@ -11,4 +11,4 @@ const connectDB = async () => {
         
     }
 };
-module.exports = mongoose.connectDB;
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const dotenv = require('dotenv');
 const colors = require('colors');
 const morgan = require('morgan');
 const cors = require('cors');
-const { connect } = require("http2");
+const connectDB = require("./config/db");
+
 
 //dot config
 dotenv.config();
@@ -27,6 +28,7 @@ app.use(morgan('dev'));
 //ROUTE
 //1 TEST ROUTE
 app.use("/api/v1/test" , require("./routes/testRoutes"));
+app.use("/api/v1/auth", require("./routes/authRoutes"));
 //port 
 const PORT= process.env.PORT || 8080;
 //listen
